28216c3bb3bc1a8ad56a7415c828a70224f4fee0
Huy Hung <hungro8102@gmail.com>
2023-11-21 05:12:52 +0700

    up up

--- App.js
+++ App.js
@@ -18,30 +18,29 @@
      
   
     return (
-      <View style={styles.container}>
-        {/* <TEST></TEST> */}
-        {/* <DoughnutChart></DoughnutChart> */}
-        {/* <Datepicker></Datepicker> */}
-      {/* <Menu1></Menu1> */}
-        <ThongKe></ThongKe>
-        {/* <Khac></Khac> */}
-        {/* <Sua></Sua> */}
-        {/* <Sua2></Sua2> */}
-        {/* <TimKiem></TimKiem> */}
+      // <View style={styles.container}>
+      //   {/* <TEST></TEST> */}
+      //   {/* <DoughnutChart></DoughnutChart> */}
+      //   {/* <Datepicker></Datepicker> */}
+      // {/* <Menu1></Menu1> */}
+      //   <ThongKe></ThongKe>
+      //   {/* <Khac></Khac> */}
+      //   {/* <Sua></Sua> */}
+      //   {/* <Sua2></Sua2> */}
+      //   {/* <TimKiem></TimKiem> */}
         
         
-      </View>
-      // <NavigationContainer>
-      //   <stack.Navigator initialRouteName='home' >
-      //     <stack.Screen name= 'Menu1' component={Menu1}  options={{ headerShown: false }}></stack.Screen>
-      //     <stack.Screen name= 'Datepicker' component={Datepicker}  options={{ headerShown: false }}></stack.Screen>
-      //     <stack.Screen name= 'Thongke' component={ThongKe}  options={{ headerShown: false }}></stack.Screen>
-      //     <stack.Screen name= 'Timkiem' component={TimKiem}  options={{ headerShown: false }}></stack.Screen>
-      //     <stack.Screen name= 'SuaTN' component={Sua}  options={{ headerShown: false }}></stack.Screen>
-      //     <stack.Screen name= 'SuaCT' component={Sua2}  options={{ headerShown: false }}></stack.Screen>
-      //     <stack.Screen name= 'Khac' component={Khac}  options={{ headerShown: false }}></stack.Screen>
-      //   </stack.Navigator>
-      // </NavigationContainer>
+      // </View>
+      <NavigationContainer>
+        <stack.Navigator initialRouteName='home' >
+          <stack.Screen name= 'Menu' component={Menu1}  options={{ headerShown: false }}></stack.Screen>
+          <stack.Screen name= 'Thongke' component={ThongKe}  options={{ headerShown: false }}></stack.Screen>
+          <stack.Screen name= 'Timkiem' component={TimKiem}  options={{ headerShown: false }}></stack.Screen>
+          <stack.Screen name= 'SuaTN' component={Sua}  options={{ headerShown: false }}></stack.Screen>
+          <stack.Screen name= 'SuaCT' component={Sua2}  options={{ headerShown: false }}></stack.Screen>
+          <stack.Screen name= 'Khac' component={Khac}  options={{ headerShown: false }}></stack.Screen>
+        </stack.Navigator>
+      </NavigationContainer>
 
     );
   }
